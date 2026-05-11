271f76edca41554307bb3e829d45018a981fee06
Krystian Sąsiadek <122701689+KrystianSA@users.noreply.github.com>
2026-05-11 19:02:05 +0200

    Add PowerShell script to create AD groups

---
+++ docs/scripts/security-groups-script.ps1
@@ -0,0 +1,4 @@
+New-ADGroup -Name "IT_Admins"     -GroupScope Global -GroupCategory Security -Path "OU=IT,DC=lab,DC=local"
+New-ADGroup -Name "Finance_Users" -GroupScope Global -GroupCategory Security -Path "OU=Finance,DC=lab,DC=local"
+New-ADGroup -Name "HR_Users"      -GroupScope Global -GroupCategory Security -Path "OU=HR,DC=lab,DC=local"
+New-ADGroup -Name "Sales_Users"   -GroupScope Global -GroupCategory Security -Path "OU=Sales,DC=lab,DC=local"
